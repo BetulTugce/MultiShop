5cfcb28e4df98a2ea976aad1916004ce471962cb
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-21 19:34:12 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+	public class ProductDetail
+	{
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Info { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Entities
+{
+	public class ProductImage
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string ProductImageID { get; set; }
+		public List<string> Images { get; set; } = new List<string>();
+
+		public string ProductId { get; set; }
+		[BsonIgnore]
+		public Product Product { get; set; }
+	}
+
+}
