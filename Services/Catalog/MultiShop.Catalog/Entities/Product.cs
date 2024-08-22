ac90489a733bcc041fdb38dc2ebbd47f20e17d71
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-22 16:00:49 +0300

    Yapılandırma ayarları düzenlendi ve settingsler yazıldı..

--- Services/Catalog/MultiShop.Catalog/Entities/Product.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -5,6 +5,9 @@ namespace MultiShop.Catalog.Entities
 {
 	public class Product
 	{
+		/* [BsonId] : MongoDBde id olarak kullanılacak prop belirtir. Bu attribute kullanılmadığında MongoDB otomatik olarak _id alanı oluşturur.
+		[BsonRepresentation(BsonType.ObjectId)] : ObjectId, MongoDB'de default ve benzersiz kimlik türüdür. Bu attribute, C# tarafında string tutulan türleri MongoDB'de ObjectId olarak temsil eder ve MongoDB'den gelen ObjectId değerlerini C# tarafında string olarak kullanmanı sağlar.
+		[BsonIgnore] : Bir özelliğin MongoDB işlemlerinde göz ardı edilmesini sağlar. Bu attributeu kullandığımız property veritabanına kaydedilmez ve veritabanından okunmaz.*/
 		[BsonId]
 		[BsonRepresentation(BsonType.ObjectId)]
 		public string Id { get; set; }
