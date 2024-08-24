b3c7c7c8861754583257d7c7a7c2cc40d4246b85
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-24 18:25:06 +0300

    Product için repository ve service yazıldı.

---
+++ Services/Catalog/MultiShop.Catalog/Repositories/Concrete/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Repositories.Abstractions;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Repositories.Concrete
+{
+	public class ProductRepository : GenericRepository<Product>, IProductRepository
+	{
+        public ProductRepository(IDatabaseSettings databaseSettings) : base(databaseSettings, databaseSettings.ProductCollectionName)
+        {
+            
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/Concrete/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Repositories.Abstractions;
+using MultiShop.Catalog.Repositories.Concrete;
+using MultiShop.Catalog.Services.Abstractions;
+
+namespace MultiShop.Catalog.Services.Concrete
+{
+	public class ProductService : IProductService
+	{
+		private readonly IProductRepository _productRepository;
+		private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+			_productRepository = productRepository;
+			_mapper = mapper;
+		}
+
+        public async Task CreateProductAsync(CreateProductDto createProductDto)
+		{
+			var product = _mapper.Map<Product>(createProductDto);
+			await _productRepository.CreateAsync(product);
+		}
+
+		public async Task DeleteProductAsync(string id)
+		{
+			await _productRepository.DeleteAsync(id);
+		}
+
+		public async Task<List<ResultProductDto>> GetAllProductsAsync()
+		{
+			var products = await _productRepository.GetAllAsync();
+			return _mapper.Map<List<ResultProductDto>>(products);
+		}
+
+		public async Task<GetByIdProductDto> GetProductByIdAsync(string id)
+		{
+			var product = await _productRepository.GetByIdAsync(id);
+			return _mapper.Map<GetByIdProductDto>(product);
+		}
+
+		public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
+		{
+			var product = _mapper.Map<Product>(updateProductDto);
+			await _productRepository.UpdateAsync(product);
+		}
+	}
+}
