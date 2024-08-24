d3ce997fdb3712e822df4484327a52354dbf21ee
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-24 22:07:21 +0300

    Veritabanı ayarları ve repositoryler DI containeraa MongoDB entegrasyonu için kaydedildi.

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,6 +1,40 @@
+using Microsoft.Extensions.Options;
+using MultiShop.Catalog.Mapping;
+using MultiShop.Catalog.Repositories.Abstractions;
+using MultiShop.Catalog.Repositories.Concrete;
+using MultiShop.Catalog.Services.Abstractions;
+using MultiShop.Catalog.Services.Concrete;
+using MultiShop.Catalog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// AutoMapper ekleniyor ve mevcut assemblydeki profiller yani mapping konfigürasyonlarý otomatik olarak yüklenir..
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+//Repository ve serviceler DI containera ekleniyor..
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductDetailRepository, ProductDetailRepository>();
+builder.Services.AddScoped<IProductImageRepository, ProductImageRepository>();
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+// appsettings.json dosyasýndaki "DatabaseSettings" adlý kýsým, veritabaný ile ilgili yapýlandýrma ayarlarýný içeriyor. appsettings.json dosyasýnda bu alanýn keyini kullanarak DatabaseSettings adlý sýnýfa baðlýyor..
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+//DatabaseSettings sýnýfýndaki deðerlere ulaþmayý saðlar..
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+	// IDatabaseSettings talep edildiðinde DI sisteminden DatabaseSettings nesnesi alýnýr..
+	return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
