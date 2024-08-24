a4ebd55eb90853e0bb7561fbaf3d7a8ca4ce5609
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-24 16:50:22 +0300

    Category Repository ve Service yazıldı.

---
+++ Services/Catalog/MultiShop.Catalog/Repositories/Concrete/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Repositories.Abstractions;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Repositories.Concrete
+{
+	public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+	{
+		public CategoryRepository(IDatabaseSettings databaseSettings)
+			: base(databaseSettings, databaseSettings.CategoryCollectionName)
+		{
+		}
+	}
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/Concrete/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Repositories.Abstractions;
+using MultiShop.Catalog.Services.Abstractions;
+
+namespace MultiShop.Catalog.Services.Concrete
+{
+	public class CategoryService : ICategoryService
+	{
+		private readonly ICategoryRepository _categoryRepository;
+		private readonly IMapper _mapper;
+
+		public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+		{
+			_categoryRepository = categoryRepository;
+			_mapper = mapper;
+		}
+
+		public async Task<List<ResultCategoryDto>> GetAllCategoriesAsync()
+		{
+			var categories = await _categoryRepository.GetAllAsync();
+			return _mapper.Map<List<ResultCategoryDto>>(categories);
+		}
+
+		public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+		{
+			var category = _mapper.Map<Category>(createCategoryDto);
+			await _categoryRepository.CreateAsync(category);
+		}
+
+		public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+		{
+			var category = _mapper.Map<Category>(updateCategoryDto);
+			await _categoryRepository.UpdateAsync(category);
+		}
+
+		public async Task DeleteCategoryAsync(string id)
+		{
+			await _categoryRepository.DeleteAsync(id);
+		}
+
+		public async Task<GetByIdCategoryDto> GetCategoryByIdAsync(string id)
+		{
+			var category = await _categoryRepository.GetByIdAsync(id);
+			return _mapper.Map<GetByIdCategoryDto>(category);
+		}
+	}
+}
