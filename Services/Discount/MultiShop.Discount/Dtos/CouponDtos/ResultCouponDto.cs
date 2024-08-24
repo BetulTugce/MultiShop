90056b0b6189355ec7860b40455710667e50df49
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-25 01:28:44 +0300

    Coupon entitysi ve dtoları oluşturuldu.

---
+++ Services/Discount/MultiShop.Discount/Dtos/CouponDtos/ResultCouponDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Discount.Dtos.CouponDtos
+{
+	public class ResultCouponDto
+	{
+		public int Id { get; set; }
+		public string Code { get; set; }
+		public int Rate { get; set; }
+		public bool IsActive { get; set; }
+		public DateTime ValidDate { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime? UpdatedDate { get; set; }
+	}
+}
