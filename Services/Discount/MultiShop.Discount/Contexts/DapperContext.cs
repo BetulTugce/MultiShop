544a1f90df5ed868a1691ecf1783baeb7622992d
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-25 14:15:29 +0300

    DapperContext sınıfı yazılarak DI containera eklendi. appsettings.json ve migration dosyaları ignore edildi.

---
+++ Services/Discount/MultiShop.Discount/Contexts/DapperContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Contexts
+{
+	public class DapperContext : DbContext
+	{
+		private readonly IConfiguration _configuration;
+		private readonly string _connectionString;
+
+		public DapperContext(IConfiguration configuration, DbContextOptions<DapperContext> options) : base(options)
+		{
+			_configuration = configuration;
+			_connectionString = _configuration.GetConnectionString("DefaultConnection");
+		}
+
+		#region Veritabanı Bağlantısı Yapılandırılması
+		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		//{
+		//	optionsBuilder.UseSqlServer("Server=your_server_name;Database=MultiShopDiscountDb;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=True;TrustServerCertificate=True;");
+		//}
+		#endregion
+
+		public DbSet<Coupon> Coupons { get; set; }
+		public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+		public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+		{
+			// ChangeTracker : Entityler üzerinden yapılan değişiklerin ya da yeni eklenen verinin yakalanmasını sağlayan ef core propertysidir. Update operasyonlarında Track edilen verileri yakalayıp elde etmemizi sağlar.
+			foreach (var entry in ChangeTracker.Entries<Coupon>())
+			{
+				if (entry.State == EntityState.Added)
+				{
+					entry.Entity.CreatedDate = DateTime.UtcNow;
+				}
+
+				if (entry.State == EntityState.Modified)
+				{
+					entry.Entity.UpdatedDate = DateTime.UtcNow;
+				}
+			}
+
+			return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+		}
+	}
+}
--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Contexts;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Veritabaný Baðlantýsý için Konfigürasyon Ayarlarý
+// DapperContext sýnýfý DI containera ekleniyor..
+builder.Services.AddDbContext<DapperContext>(options =>
+{
+	// Ýlgili ConnectionString, appsettings.json dosyasýndan alýnýyor..
+	options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServerConnection"), opt =>
+	{
+		// EfCoreun migration dosyalarýný saklayacaðý assembly yani proje belirtiliyor..
+		opt.MigrationsAssembly(Assembly.GetAssembly(typeof(DapperContext)).GetName().Name);
+	});
+});
+#endregion
 
 var app = builder.Build();
 
