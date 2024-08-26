b71d48404dcd8b1b69668d67c7cc14c7a6a0dc00
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-26 22:51:08 +0300

    Generic repository yazıldı.

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Contexts/OrderContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Persistence.Contexts
+{
+	public class OrderContext : DbContext
+	{
+        public DbSet<Domain.Entities.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
