e98deba2f08650554fee53b8f3b4602e0b6acc59
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-08-27 11:42:46 +0300

    ServiceRegistration sınıfları oluşturularak DI yapılandırması merkezileştirildi.

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/ServiceRegistration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderItemHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Persistence.Contexts;
+using MultiShop.Order.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration) 
+        {
+			#region Veritabani Baglantisi için Konfigürasyon Ayarlari
+			// OrderContext sinifi DI containera ekleniyor..
+			services.AddDbContext<OrderContext>(options =>
+			{
+				// ConnectionString, appsettings.json dosyasindan aliniyor..
+				options.UseSqlServer(configuration.GetConnectionString("MSSQLServerConnection"), opt =>
+				{
+					// EfCoreun migration dosyalarini saklayacagi assembly yani proje belirtiliyor..
+					opt.MigrationsAssembly(Assembly.GetAssembly(typeof(OrderContext)).GetName().Name);
+				});
+			});
+			#endregion
+
+			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+			#region Command ve Query Handlers
+			//CQRS handlerları dependency injection containera kaydediliyor..
+
+			services.AddScoped<GetAddressesQueryHandler>();
+			services.AddScoped<GetAddressByIdQueryHandler>();
+			services.AddScoped<CreateAddressCommandHandler>();
+			services.AddScoped<UpdateAddressCommandHandler>();
+			services.AddScoped<RemoveAddressCommandHandler>();
+
+			services.AddScoped<GetOrderItemsQueryHandler>();
+			services.AddScoped<GetOrderItemByIdQueryHandler>();
+			services.AddScoped<CreateOrderItemCommandHandler>();
+			services.AddScoped<UpdateOrderItemCommandHandler>();
+			services.AddScoped<RemoveOrderItemCommandHandler>();
+			#endregion
+		}
+	}
+}
--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,46 +1,17 @@
-using Microsoft.EntityFrameworkCore;
-using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
-using MultiShop.Order.Application.Features.CQRS.Handlers.OrderItemHandlers;
-using MultiShop.Order.Application.Interfaces;
-using MultiShop.Order.Persistence.Contexts;
-using MultiShop.Order.Persistence.Repositories;
-using System.Reflection;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
-builder.Services.AddScoped<GetAddressesQueryHandler>();
-builder.Services.AddScoped<GetAddressByIdQueryHandler>();
-builder.Services.AddScoped<CreateAddressCommandHandler>();
-builder.Services.AddScoped<UpdateAddressCommandHandler>();
-builder.Services.AddScoped<RemoveAddressCommandHandler>();
-
-builder.Services.AddScoped<GetOrderItemsQueryHandler>();
-builder.Services.AddScoped<GetOrderItemByIdQueryHandler>();
-builder.Services.AddScoped<CreateOrderItemCommandHandler>();
-builder.Services.AddScoped<UpdateOrderItemCommandHandler>();
-builder.Services.AddScoped<RemoveOrderItemCommandHandler>();
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-#region Veritabani Baglantisi için Konfigürasyon Ayarlari
-// OrderContext sinifi DI containera ekleniyor..
-builder.Services.AddDbContext<OrderContext>(options =>
-{
-	// ConnectionString, appsettings.json dosyasindan aliniyor..
-	options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServerConnection"), opt =>
-	{
-		// EfCoreun migration dosyalarini saklayacagi assembly yani proje belirtiliyor..
-		opt.MigrationsAssembly(Assembly.GetAssembly(typeof(OrderContext)).GetName().Name);
-	});
-});
-#endregion
 
 var app = builder.Build();
 
