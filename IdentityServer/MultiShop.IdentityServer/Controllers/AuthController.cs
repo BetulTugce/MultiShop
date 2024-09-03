5a446f61e37c87a258e5035816e5c285d6d9f3d1
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-03 10:47:25 +0300

    Kullanıcı kayıt işlemi için dto ve auth api controller oluşturuldu.

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public AuthController(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpPost("[action]")]
+		public async Task<IActionResult> SignUp(UserRegisterDto userRegisterDto)
+		{
+			var newUser = new ApplicationUser()
+			{
+				Email = userRegisterDto.Email,
+				UserName = userRegisterDto.Username,
+				Name = userRegisterDto.Name,
+				Surname = userRegisterDto.Surname
+			};
+			var results = await _userManager.CreateAsync(newUser, userRegisterDto.Password);
+			if (results.Succeeded)
+			{
+				return StatusCode(StatusCodes.Status201Created);
+			}
+
+			var errors = results.Errors.Select(e => e.Description).ToList();
+			return BadRequest(errors);
+		}
+	}
+}
