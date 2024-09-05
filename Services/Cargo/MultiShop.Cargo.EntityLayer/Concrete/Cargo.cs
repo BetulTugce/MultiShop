e3744bf0b9184c3829b1f3a42e36c0c68b654e07
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-05 15:55:24 +0300

    entityler oluşturuldu.

---
+++ Services/Cargo/MultiShop.Cargo.EntityLayer/Concrete/Cargo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.EntityLayer.Concrete
+{
+	public class Cargo
+	{
+		public Guid Id { get; set; }
+
+		public string Sender { get; set; }
+		public string Receiver { get; set; }
+
+		public int TrackingNumber { get; set; } // Takip Numarası
+
+        public DateTime ShippingDate { get; set; }
+
+        public Guid CargoCompanyId { get; set; } // Foreign Key
+		public CargoCompany CargoCompany { get; set; } // Kargo Şirketi
+	}
+}
