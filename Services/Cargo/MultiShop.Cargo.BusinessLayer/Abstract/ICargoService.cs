69b77a4ae299d61530baba7b7f87fb942f1485b7
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-05 17:50:48 +0300

    Business katmanında generic ve custom interfaceler oluşturuldu.

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/ICargoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract
+{
+	public interface ICargoService : IGenericService<MultiShop.Cargo.EntityLayer.Concrete.Cargo>
+	{
+	}
+}
---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract
+{
+	public interface IGenericService<T> where T : class
+	{
+		// IGenericDal'daki methodlar..
+
+		#region Read Methodları
+
+		Task<T> GetByIdAsync(Guid id);
+
+		Task<IEnumerable<T>> GetAllAsync();
+
+		IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+		Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+		// Belirtilen koşulu sağlayan ilk öğeyi asenkron olarak getirir.
+		Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true);
+
+		#endregion
+
+		#region Write Methodları
+
+		Task<T> InsertAsync(T entity);
+
+		Task UpdateAsync(T entity);
+
+		Task RemoveAsync(T entity);
+
+		#endregion
+	}
+}
