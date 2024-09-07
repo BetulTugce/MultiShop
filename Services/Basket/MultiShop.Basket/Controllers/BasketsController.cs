10a0b6825369ad083b7277f2f240086030ed5a25
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-07 12:50:36 +0300

    sepet için api controller yazıldı.

--- Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services.Abstract;
 
 namespace MultiShop.Basket.Controllers
 {
@@ -7,5 +10,39 @@ namespace MultiShop.Basket.Controllers
 	[ApiController]
 	public class BasketsController : ControllerBase
 	{
+		private readonly IBasketService _basketService;
+		private readonly ILoginService _loginService;
+
+		public BasketsController(IBasketService basketService, ILoginService loginService)
+		{
+			_basketService = basketService;
+			_loginService = loginService;
+		}
+
+		// Kullanıcının sepetini getirir..
+		[HttpGet]
+		public async Task<IActionResult> GetMyBasket() 
+		{
+			//var user = User.Claims;
+			var values = await _basketService.GetBasketAsync(_loginService.GetUserId);
+			return Ok(values);
+		}
+
+		// Kullanıcının sepetini kaydeder..
+		[HttpPost]
+		public async Task<IActionResult> SaveMyBasket(BasketTotalDto dto)
+		{
+			dto.UserId = _loginService.GetUserId;
+			await _basketService.SaveBasketAsync(dto);
+			return Ok();
+		}
+
+		// Kullanıcının sepetini siler..
+		[HttpDelete]
+		public async Task<IActionResult> DeleteBasket()
+		{
+			await _basketService.DeleteBasketAsync(_loginService.GetUserId);
+			return NoContent();
+		}
 	}
 }
