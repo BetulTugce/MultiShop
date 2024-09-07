21dbb48a37bd8e93dd510e44b67e27271bfb1bca
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-07 12:38:08 +0300

    Redis ile bağlantıyı ve veritabanı erişimini yönetmek için RedisService sınıfı oluşturuldu.

---
+++ Services/Basket/MultiShop.Basket/Settings/RedisService.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MultiShop.Basket.Settings
+{
+	public class RedisService
+	{
+		// Bağlantı işlemleri sırasında ihtiyaç duyulan parametreler (Redis sunucusunun host ve port bilgilerini tutuyorlar)..
+		private readonly string _host;
+		private readonly int _port;
+
+		// Redis bağlantısını yönetecek nesne
+		// ConnectionMultiplexer sınıfı, Redis ile yapılan tüm işlemleri yöneten ve sunucuyla iletişimi sürdüren ana bileşendir.
+		private ConnectionMultiplexer _multiplexer;
+
+		public RedisService(string host, int port)
+		{
+			_host = host;
+			_port = port;
+		}
+
+		// Redis sunucusuna bağlanma işlemi gerçekleştirir..
+		public void Connect()=> _multiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+
+		// Redis veritabanına erişim sağlar (Rediste birden fazla veritabanı olabilir (0'dan 15'e). Varsayılan olarak db 0 kullanılır)..
+		public IDatabase GetDb(int db = 1) => _multiplexer.GetDatabase(0);
+		//public IDatabase GetDb(int db = 1) => _multiplexer.GetDatabase(db);
+	}
+}
---
+++ Services/Basket/MultiShop.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Basket.Settings
+{
+    // Redis bağlantı ayarlarını tutar..
+	public class RedisSettings
+	{
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
