11a01afd3b36a3762ccb6290241230b071b75b6d
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-07 12:53:12 +0300

    Program.csde authentication, authorization ve redis ile ilgili gerekli yapılandırmalar gerçekleştirildi.

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -37,6 +37,11 @@ namespace MultiShop.IdentityServer
 				Scopes={"CargoFullPermission"}
 			},
 
+			new ApiResource("ResourceBasket")
+			{
+				Scopes={"BasketFullPermission"}
+			},
+
 			new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
 
 		};
@@ -57,6 +62,7 @@ namespace MultiShop.IdentityServer
 			new ApiScope("DiscountFullPermission", "Full authority for discount operations"),
 			new ApiScope("OrderFullPermission", "Full authority for order operations"),
 			new ApiScope("CargoFullPermission", "Full authority for cargo operations"),
+			new ApiScope("BasketFullPermission", "Full authority for basket operations"),
 			new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
 		};
 
@@ -82,7 +88,7 @@ namespace MultiShop.IdentityServer
 			{
 				ClientId = "MultiShopManagerId",
 				ClientName = "Multi Shop Manager User",
-				AllowedGrantTypes = GrantTypes.ClientCredentials,
+				AllowedGrantTypes = GrantTypes.ResourceOwnerPassword, // Giriş yapan kullanıcının parolasına göre
 				ClientSecrets = {new Secret("multishopsecret".Sha256())},
 				AllowedScopes= { "CatalogFullPermission", "DiscountFullPermission" }
 			},
@@ -92,9 +98,9 @@ namespace MultiShop.IdentityServer
 			{
 				ClientId = "MultiShopAdminId",
 				ClientName = "Multi Shop Admin User",
-				AllowedGrantTypes = GrantTypes.ClientCredentials,
+				AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
 				ClientSecrets = {new Secret("multishopsecret".Sha256())},
-				AllowedScopes= {"CatalogReadPermission", "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission", "CargoFullPermission",
+				AllowedScopes= {"CatalogReadPermission", "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission", "CargoFullPermission", "BasketFullPermission",
 				IdentityServerConstants.LocalApi.ScopeName, // IdentityServer projesindeki yerel API controllerlarına erişim yetkisi..
 				IdentityServerConstants.StandardScopes.Email,
 				IdentityServerConstants.StandardScopes.OpenId,
--- Services/Basket/MultiShop.Basket/Program.cs
+++ Services/Basket/MultiShop.Basket/Program.cs
@@ -1,8 +1,56 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Options;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services.Abstract;
+using MultiShop.Basket.Services.Concrete;
+using MultiShop.Basket.Settings;
+using StackExchange.Redis;
+using System.IdentityModel.Tokens.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Bir yetkilendirme politikasý oluþturur. Burada, sadece kimliði doðrulanmýþ kullanýcýlarýn eriþimine izin verilir (Proje seviyesinde authentication)..
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+// sub claim'ini, varsayýlan claim türleri eþleþme listesinden kaldýrýr. Bu, JWT token'ýndaki sub claim'inin, varsayýlan claim eþlemesi tarafýndan iþlenmesini engeller..
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// JWT bazlý kimlik doðrulama yapýlandýrmasý kullanýlýyor..
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+	opt.Authority = builder.Configuration["IdentityServerUrl"];
+
+	// Tokenýn geçerli olduðu kaynak adý
+	opt.Audience = "ResourceBasket";
+
+	/* Geliþtirme ortamýnda HTTPS zorunluluðunu devre dýþý býrakmak için false olarak ayarlanabilir yani, IdentityServerý geliþtirme ortamýnda ayaða kaldýrýrken https://localhost:5001 þeklinde https zorunlu olmasýn isteniyorsa bu özellik false verilebilir.. */
+	opt.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddHttpContextAccessor(); // HttpContexte eriþim için..
+builder.Services.AddScoped<ILoginService, LoginService>(); // Scoped ile her HTTP isteði baþýna bir örnek oluþturur..
+builder.Services.AddScoped<IBasketService, BasketService>();
+// RedisSettings konfigürasyon ayarlarýný appsettings.json dosyasýndan okur.
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+// Singleton ile tüm uygulama boyunca ayný nesnenin kullanýlmasý saðlanýyor yani tekrar tekrar redis baðlantýsý oluþturmak yerine mevcut baðlantý üzerinden iþlemleri gerçekleþtiriyor..
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+	// appsettings.jsondaki RedisSettings alanýndaki konfigürasyon ayarlarýný alýr..
+	var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+	var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+	redis.Connect(); // Redis sunucusuna baðlantýyý baþlatýr (Baðlantý kurma iþlemi RedisService sýnýfýnda tanýmlandý).
+	return redis; // RedisService örneðini döndürerek DI konteynýra kaydedilerek uygulama boyunca kullanýlýr..
+});
+
+builder.Services.AddControllers(opt =>
+{
+	// Yetkilendirme filtresi ekleniyor.. Tüm eylemler için, kimliði doðrulanmýþ kullanýcýlarýn eriþimini zorunlu hale getirir..
+	opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +66,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Kimlik doðrulama ve yetkilendirme middlewarelarý..
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
