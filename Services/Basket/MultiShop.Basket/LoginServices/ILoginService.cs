6f0d12e26c61dac8204b27914206290469961f5a
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-07 12:47:34 +0300

    Giriş yapan kullanıcının idsini yakalamak (auth) için loginservice sınıfı oluşturuldu.

---
+++ Services/Basket/MultiShop.Basket/LoginServices/ILoginService.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Basket.LoginServices
+{
+	public interface ILoginService
+	{
+		// Kullanıcının idsini (userId) alacak..
+		public string GetUserId { get; } // Sadece userIdye erişilecek atama yapılmayacağı için sete ihtiyaç yok.
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Claims;
+
+namespace MultiShop.Basket.LoginServices
+{
+	public class LoginService : ILoginService
+	{
+		// HttpContext'e erişim sağlamak için..
+		private readonly IHttpContextAccessor _contextAccessor;
+
+		public LoginService(IHttpContextAccessor contextAccessor)
+		{
+			_contextAccessor = contextAccessor;
+		}
+
+		//public string GetUserId
+		//{
+		//	get
+		//	{
+		//		var httpContext = _contextAccessor.HttpContext;
+
+		//		if (httpContext?.User == null)
+		//		{
+		//			throw new InvalidOperationException("User is not authenticated.");
+		//		}
+
+		//		var userId = httpContext.User.FindFirst("sub")?.Value;
+
+		//		if (string.IsNullOrEmpty(userId))
+		//		{
+		//			throw new InvalidOperationException("User ID claim is not available.");
+		//		}
+
+		//		return userId;
+		//	}
+		//}
+
+
+		//public string GetUserId => _contextAccessor.HttpContext.User.FindFirst("sub").Value;
+
+		// userid bilgisini HttpContext üzerinden alıyor..
+		public string GetUserId => _contextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+	}
+}
