ac555ee2e2ff51ed850ac0a13e1727e1265dbb94
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-08 12:10:12 +0300

    Anasayfa için mvc controller ve razor view oluşturuldu.

---
+++ Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    // Anasayfayı temsil ediyor..
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
