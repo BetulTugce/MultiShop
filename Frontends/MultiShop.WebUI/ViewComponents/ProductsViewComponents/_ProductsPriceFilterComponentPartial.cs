1ea0e12ac0d3aa3720958fbfb1121c58c8666bf3
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-10 10:14:10 +0300

    Ürünleri fiyata göre filtreleme alanı viewcomponent haline getirildi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductsViewComponents/_ProductsPriceFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductsViewComponents
+{
+    public class _ProductsPriceFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
