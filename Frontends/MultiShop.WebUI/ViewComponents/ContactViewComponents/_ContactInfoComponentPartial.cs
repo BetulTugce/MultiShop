08fe9a25f6009ca948e386d1cb1178c7df5173e3
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-10 14:25:00 +0300

    İletişim sayfası oluşturuldu, info alanı viewcomponent haline getirildi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ContactViewComponents/_ContactInfoComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ContactViewComponents
+{
+    public class _ContactInfoComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
