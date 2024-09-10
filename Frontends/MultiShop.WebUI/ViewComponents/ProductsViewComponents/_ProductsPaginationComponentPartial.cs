dcc47fea306fbcc90f24c97ac156e633cf9f283c
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-10 10:49:01 +0300

    Sayfalama alanı viewcomponent haline getirildi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductsViewComponents/_ProductsPaginationComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductsViewComponents
+{
+    public class _ProductsPaginationComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
