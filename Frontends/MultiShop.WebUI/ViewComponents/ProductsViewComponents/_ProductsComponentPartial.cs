9ef066a0d1b245e83fe150dafc4bdf115a7036e4
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-10 10:45:05 +0300

    Ürünlerin listelendiği alan viewcomponent haline getirildi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductsViewComponents/_ProductsComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductsViewComponents
+{
+    public class _ProductsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
