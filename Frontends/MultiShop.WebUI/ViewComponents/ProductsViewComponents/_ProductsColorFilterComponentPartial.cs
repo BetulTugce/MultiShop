8f183e1d52d9f0cfc4effc9c09d52b8125a2a17a
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-10 10:24:33 +0300

    Ürülerin renk ve ebata göre filtrelendiği alanlar viewcomponent haline getirildi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductsViewComponents/_ProductsColorFilterComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductsViewComponents
+{
+    public class _ProductsColorFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
