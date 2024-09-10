45f3c6615cb069cd58c4763703054c6f418ea7f0
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-10 10:40:37 +0300

    sıralama ve ürün sayısına göre filtreleme alanı viewcomponent haline getirildi.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductsViewComponents/_ProductsSortingComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductsViewComponents
+{
+    public class _ProductsSortingComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
