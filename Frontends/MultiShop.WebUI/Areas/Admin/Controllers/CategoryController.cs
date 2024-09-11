11446ebe334f4d25eb0ddef949a164db7ffe8668
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-11 12:37:04 +0300

    Contentheader alanı viewcomponent haline getirilerek değerleri dinamik alacak şekilde güncellendi.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,15 @@
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         public IActionResult Index()
         {
+            ViewBag.v0 = "Kategoriler";
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
             return View();
         }
     }
