6994c35039adfe129f458311475ec7efba518a7d
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-14 22:19:47 +0300

    Kategorileri listeleme işlemi gerçekleştirildi.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Catalog.Category;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        // IHttpClientFactory nesnesi için field tanımı.. Api istekleri yapmak için httpclient nesnesi üretmeyi sağlar..
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Dinamik olarak verileri taşımak için ViewBag kullanılıyor..
             ViewBag.v0 = "Kategoriler";
             ViewBag.v1 = "Anasayfa";
             ViewBag.v2 = "Kategoriler";
             ViewBag.v3 = "Kategori Listesi";
+
+            // HTTP istekleri için bir HttpClient oluşturuluyor..
+            var client = _httpClientFactory.CreateClient();
+            // APIdan kategorileri almak için bir GET isteği gönderiliyor..
+            var responseMessage = await client.GetAsync("https://localhost:44326/api/Categories");
+            // Eğer istek başarılıysa (HTTP 200 gibi bir başarı kodu döndüyse) bu blok çalışır.
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                // Gelen yanıtın içeriği string formatında okunuyor..
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                // JSON formatındaki veriyi CategoryListVM türünde bir listeye dönüştürülüyor..
+                var values = JsonConvert.DeserializeObject<List<CategoryListVM>>(jsonData);
+                // Verileri View ile birlikte döndürüyor..
+                return View(values);
+            }
+            // Eğer istek başarısız olursa (örneğin api kapalıysa ya da yanıt 500 dönüyorsa), boş bir View döndürüyor..
             return View();
         }
     }
---
+++ Frontends/MultiShop.WebUI/Models/ViewModels/Catalog/Category/CategoryListVM.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.WebUI.Models.ViewModels.Catalog.Category
+{
+    public class CategoryListVM
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
