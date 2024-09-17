eb16044ee7736554d7104230a4406e3c9ee1aa9d
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-17 11:36:51 +0300

    Ürün ekleme işlemi gerçekleştirildi.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiShop.WebUI.Models.ViewModels.Catalog.Category;
 using MultiShop.WebUI.Models.ViewModels.Catalog.Product;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
@@ -40,6 +43,51 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 return View(values);
             }
             // Eğer istek başarısız olursa (örneğin api kapalıysa ya da yanıt 500 dönüyorsa), boş bir View döndürüyor..
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct()
+        {
+            // Dinamik olarak verileri taşımak için ViewBag kullanılıyor..
+            ViewBag.v0 = "Ürünler";
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Yeni Ürün";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44326/api/Categories");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var categories = JsonConvert.DeserializeObject<List<CategoryListVM>>(jsonData);
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name");
+            //List<SelectListItem> categoryList = (from item in categories
+            //                                     select new SelectListItem
+            //                                     {
+            //                                         Text = item.Name, // Dropdownda görünecek kısım categorynin adı..
+            //                                         Value = item.Id
+            //                                     }).ToList();
+            //ViewBag.CategoryList = categoryList;
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductCreateVM productCreateVM)
+        {
+            var client = _httpClientFactory.CreateClient();
+            // productCreateVM modeli JSON formatına dönüştürülüyor..
+            var jsonData = JsonConvert.SerializeObject(productCreateVM);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44326/api/Products", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            else if (responseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                //TODO: Login sayfasına yönlendirilecek..
+                return View();
+            }
             return View();
         }
     }
---
+++ Frontends/MultiShop.WebUI/Models/ViewModels/Catalog/Product/ProductCreateVM.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.WebUI.Models.ViewModels.Catalog.Product
+{
+    public class ProductCreateVM
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+
+        public string CategoryId { get; set; }
+    }
+}
