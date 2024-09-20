e993e04dc3b2f794a3c05c80b1050b2c8e92112b
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-21 01:36:36 +0300

    Navbarda kategorileri listeleme işlemi gerçekleştirildi.

--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Catalog.Category;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _NavbarUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _NavbarUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // HTTP istekleri için bir HttpClient oluşturuluyor..
+            var client = _httpClientFactory.CreateClient();
+            // APIdan kategorileri almak için bir GET isteği gönderiliyor..
+            var responseMessage = await client.GetAsync("https://localhost:44326/api/Categories/");
+            // Eğer istek başarılıysa (HTTP 200 gibi bir başarı kodu döndüyse) bu blok çalışır.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // Gelen yanıtın içeriği string formatında okunuyor..
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                // JSON formatındaki veriyi CategoryListVM türünde bir listeye dönüştürülüyor..
+                var values = JsonConvert.DeserializeObject<List<CategoryListVM>>(jsonData);
+                // Verileri View ile birlikte döndürüyor..
+                return View(values);
+            }
+            // Eğer istek başarısız olursa (örneğin api kapalıysa ya da yanıt 500 dönüyorsa), boş bir View döndürüyor..
             return View();
         }
     }
