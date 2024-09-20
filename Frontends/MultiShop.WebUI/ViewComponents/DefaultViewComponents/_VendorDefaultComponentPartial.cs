22207d1bbb7f6addb7b020cd17a53e3b39fb55ba
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-20 20:06:42 +0300

    Kullanıcı panelinde anasayfadaki markalar bölümü dinamik hale getirildi.

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_VendorDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_VendorDefaultComponentPartial.cs
@@ -1,9 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Catalog.Brand;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _VendorDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        // IHttpClientFactory nesnesi için field tanımı.. Api istekleri yapmak için httpclient nesnesi üretmeyi sağlar..
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _VendorDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            // HTTP istekleri için bir HttpClient oluşturuluyor..
+            var client = _httpClientFactory.CreateClient();
+            // APIdan markaları almak için bir GET isteği gönderiliyor..
+            var responseMessage = await client.GetAsync("https://localhost:44326/api/Brands");
+            // Eğer istek başarılıysa (HTTP 200 gibi bir başarı kodu döndüyse) bu blok çalışır.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // Gelen yanıtın içeriği string formatında okunuyor..
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                // JSON formatındaki veriyi BrandListVM türünde bir listeye dönüştürülüyor..
+                var values = JsonConvert.DeserializeObject<List<BrandListVM>>(jsonData);
+                // Verileri View ile birlikte döndürüyor..
+                return View(values);
+            }
+            // Eğer istek başarısız olursa (örneğin api kapalıysa ya da yanıt 500 dönüyorsa), boş bir View döndürüyor..
+            return View();
+        }
     }
 }
