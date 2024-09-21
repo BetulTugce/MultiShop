cf0916207e5804a6e2a5ea7f3232d1bdce52cf7b
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-21 19:35:12 +0300

    Kullanıcı panelinde ürüne ait görselleri getirme işlemi gerçekleştirildi.

---
+++ Frontends/MultiShop.WebUI/Models/ViewModels/Catalog/ProductImage/ProductImageVM.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.WebUI.Models.ViewModels.Catalog.ProductImage
+{
+    public class ProductImageVM
+    {
+        public string Id { get; set; }
+        public List<string> Images { get; set; } = new List<string>();
+
+        public string ProductId { get; set; }
+    }
+}
--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailImageSliderComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailImageSliderComponentPartial.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Catalog.ProductImage;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailImageSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        // IHttpClientFactory nesnesi için field tanımı.. Api istekleri yapmak için httpclient nesnesi üretmeyi sağlar..
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailImageSliderComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id) 
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44326/api/ProductImages/GetProductImageByProductId/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ProductImageVM>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
