3de579877c6353da8d455f7df0a1189fbd4ff796
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-26 01:45:54 +0300

    Ürün detay sayfasındaki açıklama ve bilgi partialviewları dinamik hale getirildi.

---
+++ Frontends/MultiShop.WebUI/Models/ViewModels/Catalog/ProductDetail/ProductDetailVM.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.WebUI.Models.ViewModels.Catalog.ProductDetail
+{
+    public class ProductDetailVM
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Info { get; set; }
+
+        public string ProductId { get; set; }
+    }
+}
--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailInformationComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailInformationComponentPartial.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Catalog.ProductDetail;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailInformationComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() {  return View(); }
+        // IHttpClientFactory nesnesi için field tanımı.. Api istekleri yapmak için httpclient nesnesi üretmeyi sağlar..
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailInformationComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string productId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44326/api/ProductDetails/GetProductDetailByProductId/" + productId);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var prodDetail = JsonConvert.DeserializeObject<ProductDetailVM>(jsonData);
+                return View(prodDetail);
+            }
+            return View();
+        }
     }
 }
