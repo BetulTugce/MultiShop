6b069c0ba40a29bbc40a6af9effa44396fdea47a
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-25 20:44:26 +0300

    productdetail güncelleme işlemleri tamamlandı.

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductDetailController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Catalog.ProductDetail;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/ProductDetail/")]
+    public class ProductDetailController : Controller
+    {
+        // IHttpClientFactory nesnesi için field tanımı.. Api istekleri yapmak için httpclient nesnesi üretmeyi sağlar..
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductDetailController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("UpdateProductDetail")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateProductDetail(ProductDetailUpdateVM productDetailUpdateVM)
+        {
+            var client = _httpClientFactory.CreateClient();
+            // productDetailUpdateVM modeli JSON formatına dönüştürülüyor..
+            var jsonData = JsonConvert.SerializeObject(productDetailUpdateVM);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44326/api/ProductDetails", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //return RedirectToAction("Index", "Product", new { area = "Admin" });
+                return RedirectToAction("GetProductsWithCategory", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
