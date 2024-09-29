02c0fe42e052126ebb21bed8c08f5e22ef8e1a3b
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-29 19:36:20 +0300

    Register sayfası ayağa kaldırıldı.

--- Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
+++ Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models.ViewModels.Identity.Register;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class RegisterController : Controller
     {
-        [HttpGet]
+		private readonly IHttpClientFactory _httpClientFactory;
+		public RegisterController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		public async Task<IActionResult> Index(RegisterCreateVM createVM)
+		{
+			if (createVM.ConfirmPassword == createVM.Password)
+			{
+				var client = _httpClientFactory.CreateClient();
+				var jsonData = JsonConvert.SerializeObject(createVM);
+				StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+				var responseMessage = await client.PostAsync("https://localhost:5001/api/Auth/SignUp", stringContent);
+				if (responseMessage.IsSuccessStatusCode)
+				{
+					return RedirectToAction("Index", "Login");
+				}
+			}
+			return View(createVM);
+		}
+	}
 }
---
+++ Frontends/MultiShop.WebUI/Models/ViewModels/Identity/Register/RegisterCreateVM.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.WebUI.Models.ViewModels.Identity.Register
+{
+	public class RegisterCreateVM
+	{
+		public string Username { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string ConfirmPassword { get; set; }
+	}
+}
