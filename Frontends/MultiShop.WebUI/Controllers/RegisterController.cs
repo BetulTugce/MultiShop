05f8f0a8a8b1319457f373df3dae025517828cf6
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-29 12:16:52 +0300

    Register form oluşturuldu.

---
+++ Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
