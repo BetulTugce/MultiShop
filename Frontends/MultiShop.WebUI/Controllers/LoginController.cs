e0ace6a9e845c9dfe16660c31f77480d652d743d
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2024-09-29 19:58:37 +0300

    Login sayfası oluşturuldu.

---
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
